66ef2ce1373fe64eac42939b97e83c570c5ef0f2
sunjingjing <e1237234@u.nus.edu>
2024-11-20 19:09:59 +0800

    modify data_split implementation

--- train_and_evaluate.py
+++ train_and_evaluate.py
@@ -2,8 +2,8 @@ from utils.metrics import calculate_rating_metrics, calculate_ranking_metrics
 from models.svd_model import SVDModel
 from models.lightgcn_model import LightGCNModel
 
-def svd_model_train_and_evaluate(size):
-    svd_model = SVDModel(size, n_factors=200, n_epochs=30)
+def svd_model_train_and_evaluate(train_set, test_set):
+    svd_model = SVDModel(train_set=train_set, test_set=test_set, n_factors=200, n_epochs=30)
     svd_model.prepare_training_data()
     svd_model.train()
     predictions = svd_model.predict()
@@ -15,9 +15,10 @@ def svd_model_train_and_evaluate(size):
 
     return ratings, rankings
 
-def lgcn_model_train_and_evaluate(size):
+def lgcn_model_train_and_evaluate(train_set, test_set):
     lgcn_model = LightGCNModel(
-        size=size,
+        train_set=train_set,
+        test_set=test_set,
         num_layers=3,
         num_negatives=4,
         embedding_dim=64,
@@ -26,6 +27,7 @@ def lgcn_model_train_and_evaluate(size):
         batch_size=1024,
         device='cpu'
     )
+
     lgcn_model.prepare_training_data()
     lgcn_model.train()
     predictions = lgcn_model.predict()
--- utils/data_split.py
+++ utils/data_split.py
@@ -1,70 +1,22 @@
 import numpy as np
 import math
-from sklearn.model_selection import train_test_split as sk_split
+import pandas as pd
+    
+def train_test_split(data, ratio=0.75, seed=42):
+    train_list = []
+    test_list = []
 
-def split_pandas_data_with_ratios(data, ratios, seed=42, shuffle=False):
+    grouped = data.groupby('user')
+    for user, group in grouped:
+        if len(group) < 2:
+            train_list.append(group)
+        else:
+            test_sample = group.sample(n=int((1-ratio) * len(group)), random_state=42)
+            train_sample = group.drop(test_sample.index)
+            train_list.append(train_sample)
+            test_list.append(test_sample)
 
-    if math.fsum(ratios) != 1.0:
-        raise ValueError("The ratios have to sum to 1")
-
-    split_index = np.cumsum(ratios).tolist()[:-1]
-
-    if shuffle:
-        data = data.sample(frac=1, random_state=seed)
-
-    splits = np.split(data, [round(x * len(data)) for x in split_index])
-
-    # Add split index (this makes splitting by group more efficient).
-    for i in range(len(ratios)):
-        splits[i]["split_index"] = i
-
-    return splits
-
-def process_split_ratio(ratio):
-    if isinstance(ratio, float):
-        if ratio <= 0 or ratio >= 1:
-            raise ValueError("Split ratio has to be between 0 and 1")
-
-        multi = False
-    elif isinstance(ratio, list):
-        if any([x <= 0 for x in ratio]):
-            raise ValueError(
-                "All split ratios in the ratio list should be larger than 0."
-            )
-
-        # normalize split ratios if they are not summed to 1
-        if math.fsum(ratio) != 1.0:
-            ratio = [x / math.fsum(ratio) for x in ratio]
-
-        multi = True
-    else:
-        raise TypeError("Split ratio should be either float or a list of floats.")
-
-    return multi, ratio
-
-def python_random_split(data, ratio=0.75, seed=42):
-    """Pandas random splitter.
-
-    The splitter randomly splits the input data.
-
-    Args:
-        data (pandas.DataFrame): Pandas DataFrame to be split.
-        ratio (float or list): Ratio for splitting data. If it is a single float number
-            it splits data into two halves and the ratio argument indicates the ratio
-            of training data set; if it is a list of float numbers, the splitter splits
-            data into several portions corresponding to the split ratios. If a list is
-            provided and the ratios are not summed to 1, they will be normalized.
-        seed (int): Seed.
-
-    Returns:
-        list: Splits of the input data as pandas.DataFrame.
-    """
-    multi_split, ratio = process_split_ratio(ratio)
-
-    if multi_split:
-        splits = split_pandas_data_with_ratios(data, ratio, shuffle=True, seed=seed)
-        splits_new = [x.drop("split_index", axis=1) for x in splits]
-
-        return splits_new
-    else:
-        return sk_split(data, test_size=None, train_size=ratio, random_state=seed)
+    train_data = pd.concat(train_list, ignore_index=True)
+    test_data = pd.concat(test_list, ignore_index=True)
+    return train_data, test_data
+    
